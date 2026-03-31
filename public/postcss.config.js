1c841d473e5467d3994ef57144fd5e4be7653c45
baptxu <bastougmg@gmail.com>
2026-03-31 20:02:43 +0200

    fix postcss config

---
+++ public/postcss.config.js
@@ -0,0 +1,13 @@
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+```
+
+**Ctrl+S** puis dans le terminal :
+```
+git add .
+git commit -m "fix postcss config"
+git push
